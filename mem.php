a7292cdcdd1c96d55390370e8c98d97f3bd0e50d
hserver71 <sprite12131@outlook.com>
2026-04-25 08:59:05 -0500

    fetch api changed to the simple ones

--- mem.php
+++ mem.php
@@ -38,12 +38,14 @@ if($action == "start"){
     }
 }else if($action == "show"){
     $updated_time = apcu_fetch('last_updated');
-    echo "Updated time: " . date('Y-m-d H:i:s', $updated_time) . "\n";
+    echo "Updated time: " . date('Y-m-d H:i:s', $updated_time) . "\n\n";
+    echo "Meta data: " . json_encode(apcu_fetch('meta_data')) . "\n\n";
     echo "Data: " . json_encode(apcu_fetch('data')) . "\n";
 }else if($action == "clear"){
     apcu_delete('data');
+    apcu_delete('meta_data');
     apcu_delete('last_updated');
-    echo "Data cleared";
+    echo "Cache cleared successfully!";
 }else{
     echo "Invalid action";
 }
